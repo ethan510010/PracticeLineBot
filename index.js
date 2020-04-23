0be6967404843def6b86bf8a556b46320942778b
EthanLin <ethan510010@gmail.com>
2020-04-23 19:45:14 +0800

    learning basic linebot

---
+++ index.js
@@ -0,0 +1,21 @@
+const linebot = require('linebot');
+require('dotenv').config();
+
+const bot = linebot({
+  channelId: process.env.channelID,
+  channelSecret: process.env.channelSecret,
+  channelAccessToken: process.env.channelAccessToken
+})
+
+bot.on('message', async (event) => {
+  const originalMessage = event.message.text;
+  try {
+    await event.reply(`User says: ${originalMessage}`);
+  } catch (error) {
+    throw error;
+  }
+})
+
+bot.listen('/linewebhook', 3000, () => {
+  console.log('Bot starts');
+})
